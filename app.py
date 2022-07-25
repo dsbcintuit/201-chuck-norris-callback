3b494c0857954f2ec741b83109db6419ff2c1f55
dsbcintuit <sabrina_carbo@intuit.com>
2022-07-25 23:59:16 +0000

    from scratch

--- app.py
+++ app.py
@@ -4,34 +4,12 @@ from dash import dcc, html, Input, Output, State
 import os
 
 ###### Set up variables
-list_of_choices=[
-    {
-        "label": "Sing",
-        "image": "mainguy.jpeg"
-    },
-    {
-        "label": "Landlady",
-        "image": "landlady.jpeg"
-    },
-    {
-        "label": "Landlord",
-        "image": "husband.jpeg"
-    },
-    {
-        "label": "The Beast",
-        "image": "frog.jpeg"
-    },
-    {
-        "label": "The Tailor",
-        "image": "ringsguy.jpeg"
-    }
-]
- 
+list_of_choices=['punch', 'body-slam', 'round-house kick to the face']
+list_of_images=['mainguy.jpeg', 'landlady.jpeg', 'husband.jpeg', 'frog.jpeg', 'ringsguy.jpeg']
 githublink = 'https://github.com/dsbcintuit/201-chuck-norris-callback'
+image1='poster.jpeg'
 heading1='Which Kung Fu Hustle Character Are You?'
 tabtitle='Tha Hustle'
-def_image='poster.jpeg'
-default_text="Select a character"
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -42,35 +20,25 @@ app.title=tabtitle
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
-    html.Img(src=app.get_asset_url(def_image), style={'width': '400px', 'height': '15%'}),
-    dcc.Dropdown(
-        id='your-input-here',
-        options=[{'label': list_of_choices[i], 'value': i} for i in range(len(list_of_choices))],
-        value=list_of_choices[4]),
-        # options=[{'label': i, 'value': i} for i in list_of_choices],
-        # options=[{'label': i["label"], 'value': x} for x,i in enumerate(list_of_choices)],
-        # placeholder=default_text,
-        # value=0),        
+    html.Img(id='image', src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
+    dcc.Dropdown(id='your-input-here',
+                options=[{'label': i, 'value': i} for i in list_of_choices],
+                value=list_of_choices[0],
+                style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
     html.Br(),
-    html.Img(src='', style={'width': '500px'}, id='image_choice'),
-    html.Br(),
     html.A('Code on Github', href=githublink),
+
 ])
 
 
 ######### Interactive callbacks go here #########
-@app.callback([Output('your-output-here', 'children'), 
-               Output('image_choice', 'src')
-              ],
-              [Input('your-input-here', 'value')])
-
+@app.callback([dash.dependencies.Output('your-output-here', 'children'),
+              dash.dependencies.Output('image', 'src')]
+              [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-
-    image_chosen = html.Img(src=app.get_asset_url(whatever_you_chose), style={'width': 'auto', 'height': '50%'})
-    text = list_of_choices[whatever_you_chose]["label"]
-    return f'The Kung Fu Hustle character that best represents you is {text}.', image_chosen
+    return f'Chuck Norris will now execute you with a {whatever_you_chose}.'
 
 
 ######### Run the app #########
