0b4b05ba8b15767e3c21b59e2c714a3435613fe4
dsbcintuit <sabrina_carbo@intuit.com>
2022-07-25 13:32:11 +0000

    updates

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ list_of_images=['mainguy.jpeg', 'landlady.jpeg', 'husband.jpeg', 'frog.jpeg', 'r
 heading1='Which Kung Fu Hustle Character Are You?'
 tabtitle='Tha Hustle'
 def_image='poster.jpeg'
+default_text="Select a character"
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -23,19 +24,16 @@ app.layout = html.Div([
     html.Img(src=app.get_asset_url(def_image), style={'width': '400px', 'height': '15%'}),
     dcc.Dropdown(
         id='your-input-here',
-        options=[
-                {'label':list_of_choices[0], 'value':list_of_images[0]},
-                {'label':list_of_choices[1], 'value':list_of_images[1]},
-                {'label':list_of_choices[2], 'value':list_of_images[2]},
-                {'label':list_of_choices[3], 'value':list_of_images[3]},
-                {'label':list_of_choices[4], 'value':list_of_images[4]}
-               ],
-        value=list_of_images[4],
-        style={'width': '500px'},
-        placeholder="Select a character",),        
+        options=(id='your-input-here',
+                options=[{'label': list_of_choices[i], 'value': i} for i in range(len(list_of_choices))],
+                value=list_of_choices[0],
+                style={'width': '500px'},
+        placeholder=default_text,),        
     html.Br(),
     html.Div(id='your-output-here', children=''),
     # html.Div(id='your-output-here', image=''),
+    html.Br(),
+    html.Img(id='image-output', src=app.get_asset_url(list_of_images[0])),
     html.Br(),
     html.A('Code on Github', href=githublink),
 ])
@@ -43,7 +41,7 @@ app.layout = html.Div([
 
 ######### Interactive callbacks go here #########
 @app.callback([Output('your-output-here', 'children'), 
-               Output('your-output-here', 'image')
+               Output('image-output', 'src')
               ],
               [Input('your-input-here', 'value')])
 
