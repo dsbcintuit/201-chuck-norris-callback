04581ddbff6e132f9071dbaa4f53ad167b87aed9
dsbcintuit <sabrina_carbo@intuit.com>
2022-07-26 01:11:11 +0000

    le sigh

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from dash import dcc, html, Input, Output, State
 import os
 
 ###### Set up variables
-list_of_options=['Sing', 'Landlady', 'Landlord', 'The Beast', 'The Tailor']
+list_of_choices=['Sing', 'Landlady', 'Landlord', 'The Beast', 'The Tailor']
 list_of_images=['mainguy.jpeg', 'landlady.jpeg', 'husband.jpeg', 'frog.jpeg', 'ringsguy.jpeg']
 githublink = 'https://github.com/dsbcintuit/201-chuck-norris-callback'
-# image1='poster.jpeg'
+image1='poster.jpeg'
 heading1='Which Kung Fu Hustle Character Are You?'
 tabtitle='Tha Hustle'
 
@@ -20,19 +20,12 @@ app.title=tabtitle
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
-    html.Img(id='image', src=app.get_asset_url(list_of_images[0]), style={'width': 'auto', 'height': '10%'}),
-    dcc.Dropdown(id='your-input-here',
-        options=[
-                {'label':list_of_options[0], 'value':list_of_images[0]},
-                {'label':list_of_options[1], 'value':list_of_images[1]},
-                {'label':list_of_options[2], 'value':list_of_images[2]},
-                {'label':list_of_options[3], 'value':list_of_images[3]},
-                {'label':list_of_options[4], 'value':list_of_images[4]}
-                ],
-        value=list_of_options[4],
-        placeholder='Select option...',
-        style={'width': '500px'}),
-    html.Br(),
+    html.Img(id='image-output', src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
+	    dcc.Dropdown(id='your-input-here',	
+                options=[{'label': list_of_choices[i], 'value': i} for i in range(len(list_of_choices))],	
+                value=list_of_choices[0],	
+                style={'width': '500px'}
+                placeholder='Select an option'),
     html.Div(id='your-output-here', children=''),
     html.Br(),
     html.A('Code on Github', href=githublink),
@@ -45,8 +38,14 @@ app.layout = html.Div([
               [dash.dependencies.Input('your-input-here', 'value')])
 
 def display_value(whatever_you_chose):
-    return f'The Kung Fu Hustle character that best represents you is {list_of_options[whatever_you_chose]}.'
+    return f'The Kung Fu Hustle character that best represents you is {whatever_you_chose}.'
 
+
+@app.callback(dash.dependencies.Output('image-output', 'src'),
+              [dash.dependencies.Input('your-input-here', 'value')])
+
+def display_value(whatever_you_chose):
+    return app.get_asset_url(list_of_images[whatever_you_chose])
 
 ######### Run the app #########
 if __name__ == '__main__':
